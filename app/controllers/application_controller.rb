104006069dbf04620edc622bd842799900838162
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-11 05:57:48 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -15,5 +16,33 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+  #HAndling delete requests
+  delete '/reviews/:id' do
+    #find the review using the id (using AR)
+    review = Review.find(params[:id])
+    #delete the review
+    review.destroy
+    #send a response with the deleted review as JSON
+    review.to_json 
+  end
+#Handling post (create new reviews) requests
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
 
+  end
+#patch requests
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
